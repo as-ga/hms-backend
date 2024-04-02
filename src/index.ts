dd1655436c42ca3f1588e15536fd26750a36a61f
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-02 18:07:37 +0000

    Add dotenv package and configure environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
 import express from 'express';
 
+dotenv.config({ path: "./.env" })
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hospital Management System (HMS) Backend');
